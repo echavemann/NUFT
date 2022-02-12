ddf2390384d1e700f540e1b98bef126361da5da0
echavemann <echavemann@gmail.com>
2022-02-12 17:03:24 -0600

    More Inits

---
+++ Alpaca/Alpaca.py
@@ -0,0 +1,6 @@
+import Alpaca
+
+APCA_API_KEY_ID = 'key'
+APCA_API_SECRET_KEY= 'secret'
+APCA_API_BASE_URL = 'https://paper.api.alpaca.markets' 
+#This is our paper endpoint, live endpoint is at https://data.alpaca.markets. 
---
+++ BinancePaper/binance.py
@@ -0,0 +1,6 @@
+from binance import Client, ThreadedWebsocketManager, TheadedDepthCacheManager
+api_key = ''
+api_secret = ''
+
+client = Client(api_key, api_secret)
+
