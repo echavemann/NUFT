c13282f9b6a31580f00b956393fd4f661f54ed04
echavemann <echavemann@gmail.com>
2022-03-06 17:48:13 -0600

    comment update

--- YahooFinance/YahooParser.py
+++ YahooFinance/YahooParser.py
@@ -6,37 +6,35 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
-#Sheet cpmfog
+#Sheet comfig
 url = "https://yfapi.net/v6/finance/quote"
-
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SERVICE_ACCOUNT_FILE = ('key.json')
-
 creds = None
 creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
 spreadsheetURL = 'URL'
-
 service = build('sheets', 'v4', credentials=creds)
 
-#Here we will have to fill in and/or import our ENTIRE watchlist.
+#Import from page one of the sheet.
 watchlist = {}
-#module
+
+#YF Overview
 msft = yf.Ticker("MSFT")
 msft.info #Open, H, L, C, Volume, Div, Splits, 
 msft.history() #Args:
-#Period - data to dl (1d)
-#Interval (1m)
-#start
-#end
-#prepost (Include pre and post: default to false)
-#auto_adjust (adjusts, defaults to true)
-#actions (bools dividends :)
-#Process: For each stock in our dict, call info. Rate sort those into our sheets. Repeat. 
+    #Period - data to dl (1d)
+    #Interval (1m)
+    #start
+    #end
+    #prepost (Include pre and post: default to false)
+    #auto_adjust (adjusts, defaults to true)
+    #actions (bools dividends :)
+    #Process: For each stock in our dict, call info. Rate sort those into our sheets. Repeat. 
 for stock in watchlist:
     tick = yf.Ticker(stock)
     stack = tick.info
     #store stack values
+    
 #Spreadsheet logic in progress. 
 today = ('1969-12-12')
 #Y-M-D
