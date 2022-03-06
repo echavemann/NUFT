9d2db88a6dd505e2e6145a683d5b3e929754dc36
echavemann <echavemann@gmail.com>
2022-03-06 17:35:17 -0600

    Merge branch 'master' of https://github.com/echavemann/NUFT

--- YahooFinance/YahooParser.py
+++ YahooFinance/YahooParser.py
@@ -1,8 +1,38 @@
 import requests
+import yfinance as yf
+from datetime import date
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+
+today = date.today()
+watchlist = {}
+#module
+msft = yf.Ticker("MSFT")
+msft.info #Open, H, L, C, Volume, Div, Splits, 
+msft.history() #Args:
+#Period - data to dl (1d)
+#Interval (1m)
+#start
+#end
+#prepost (Include pre and post: default to false)
+#auto_adjust (adjusts, defaults to true)
+#actions (bools dividends :)
+#Process: For each stock in our dict, call info. Rate sort those into our sheets. Repeat. 
+for stock in watchlist:
+    tick = yf.Ticker(stock)
+    stack = tick.info
+    #store stack values
+    
+    
+
+
+
+
+
+
+
 
 url = "https://yfapi.net/v6/finance/quote"
 
