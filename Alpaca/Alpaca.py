bcc73d95850e7c76b780dc730fd4506b36b80600
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2022-03-23 11:38:19 -0500

    libdate

--- Alpaca/Alpaca.py
+++ Alpaca/Alpaca.py
@@ -1,6 +1,43 @@
-import Alpaca
+from sys import api_version
+import numpy as np
+import pandas as pd
+import scipy as sci
+import alpaca_trade_api as tradeapi
+import logging
+import matplotlib as plt
+import os
 
 APCA_API_KEY_ID = 'key'
 APCA_API_SECRET_KEY= 'secret'
 APCA_API_BASE_URL = 'https://paper.api.alpaca.markets' 
-#This is our paper endpoint, live endpoint is at https://data.alpaca.markets. 
+
+api = tradeapi.REST(key_id=APCA_API_KEY_ID, secret_key=APCA_API_SECRET_KEY, base_url=APCA_API_BASE_URL, api_version= 'v2')
+account = api.get_account()
+
+def marketorder (ticker, qty):
+    qty = str(qty)
+    if '$' in qty:
+        qty = qty.remove('$')
+        tradeapi.submit_order(
+            symbol= ticker,
+            notional = qty,
+            side = 'buy',
+            type = 'market',
+            time_in_force = 'day',
+        )
+            
+    else:
+        tradeapi.submit_order(
+            symbol = ticker,
+            qty=qty,
+            side='buy',
+            type = 'market',
+            time_in_force = 'day',
+    )
+
+def outpositions():
+    portfolio = tradeapi.list_postions()
+    for position in portfolio:
+        print("{} shares of {}".format(position.qty,position.symbol))
+
+
