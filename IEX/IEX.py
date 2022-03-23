975aa3e6bc17ca49efe4e283391699c8afff5339
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2022-03-23 17:13:39 -0500

    Update IEX.py

--- IEX/IEX.py
+++ IEX/IEX.py
@@ -1,7 +1,15 @@
 import pyEX as p
 import pandas as pd
+import os
+import requests
+from iexfinance.stocks import get_historical_data
 
-symbol = 'AAPL'
-c = p.client(api_token='',version = 'sandbox')
-chart = p.chart(symbol, timeframe='1m', date = None, token ='', version = '', filter='')
-pd.DataFrame(c.batchDF(symbols=symbol, fields='chart', last=365, range_='1y')['chart'])
+api_key = 'pk_9875fe91ce264eadb369752fd783e74b'
+secret = 'sk_84a2db154d084c5291a439ca156520bc'
+
+base_url = 'https://cloud.iexapis.com/v1'
+sandbox_url = 'https://sandbox.iexapis.com/stable'
+
+df = get_historical_data("MSFT", output_format = 'pandas', token=secret)
+
+print(df)
