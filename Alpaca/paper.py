21dc36345bb70423dc184e48e76cca6a5a8571ca
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2022-03-23 11:47:01 -0500

    Update and rename Alpaca.py to paper.py

--- Alpaca/paper.py
+++ Alpaca/paper.py
@@ -9,7 +9,7 @@ import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
-from google.oauth2 import oauth
+from google.oauth2 import service_account
 
 
 APCA_API_KEY_ID = 'key'
