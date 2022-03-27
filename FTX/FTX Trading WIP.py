00397ed7a074f5d361a3c55e5db7af3eccab34ce
dpark4 <95252586+dpark4@users.noreply.github.com>
2022-03-27 16:59:45 -0500

    Add files via upload

---
+++ FTX/FTX Trading WIP.py
@@ -0,0 +1,14 @@
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+# Here's the pip for this!
+#   pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SERVICE_ACCOUNT_FILE = ('key.json')
+creds = None
+creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+spreadsheetURL = 'URL'
+service = build('sheets', 'v4', credentials=creds)
