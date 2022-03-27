b0c20c48e066a05a54c4afc627b420613183de47
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2022-03-27 18:21:48 -0500

    ftx api

--- FTX/FTX Trading WIP.py
+++ FTX/FTX Trading WIP.py
@@ -2,9 +2,24 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+import numpy as np
+import pandas as pd
+import datetime
+import time
+from ftxapi import client
+import json
+
 
 # Here's the pip for this!
 #   pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+
+endpoint = "https://ftx.com/api/markets"
+API_KEY = ""
+SECRET_KEY = ""
+
+all_markets = requests.get(endpoint_url).json()
+df = pd.DataFrame(all_markets['result'])
+df.set_index('name', inplace = True)
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SERVICE_ACCOUNT_FILE = ('key.json')
