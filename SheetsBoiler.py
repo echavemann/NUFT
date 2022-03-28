cc04689f6616123d3a62e60fd8d8409fee3caf1f
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2022-03-28 11:04:30 -0500

    Final
    Added instructions

--- SheetsBoiler.py
+++ SheetsBoiler.py
@@ -3,9 +3,11 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
-//Here's the pip for this!
-//  pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+#Here's the pip for this!
+#pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 
+#Speadsheet URL will be in GDrive
+#Make sure you grab your corresponding key off the gdrive and download it onto PATH, and then rename to "key.json". If you open the key, you should see all the authorized accounts you need. 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SERVICE_ACCOUNT_FILE = ('key.json')
 creds = None
