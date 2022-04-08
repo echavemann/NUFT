d1117310315e97a90a91d33de93bc77b8deff245
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2022-04-08 12:43:16 -0500

    Delete Glassnode directory

--- Glassnode/gnode.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pandas as pd
-import json
-import numpy
-API_KEY = ''
-
-res = requests.get('https://api.glassnode.com/v1/metrics/indicators/sopr',
-    params={'a': 'BTC', 'api_key': API_KEY})
