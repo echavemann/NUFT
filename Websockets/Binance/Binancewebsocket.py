2c5123fe958ad4ac6b0d6ba57dc6aec7b9b1cfdc
jiale0402 <55820495+jiale0402@users.noreply.github.com>
2022-04-10 17:01:22 -0500

    Update Binancewebsocket.py

--- Websockets/Binance/Binancewebsocket.py
+++ Websockets/Binance/Binancewebsocket.py
@@ -1,6 +1,8 @@
 import time
+import pandas
 
 from binance import ThreadedWebsocketManager
+
 
 api_key = ''
 api_secret = ''
@@ -11,12 +13,45 @@ def main():
     socket = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
     socket.start()
 
+    global current_list_of_dict
+    current_list_of_dict = []
+    global dict_count
+    dict_count = 0
+    global count
+    count = 0
+
     def handle_socket_message(msg):
-        print(f"message type: {msg['e']}")
-        print(msg)
+        # print(f"message type: {msg['e']}")
+        global current_list_of_dict
+        global dict_count
+        global count
+        del msg["M"]
+        current_list_of_dict.append(msg)
+        dict_count = dict_count + 1
+        if dict_count > 100: 
+            dict_count = 0
+            df = pandas.DataFrame(current_list_of_dict)
+            current_list_of_dict = []
+            df.to_csv("TradaData"+str(count)+".csv")
+            count = count + 1
 
     for i in range(len(symbols)):  
         socket.start_aggtrade_socket(callback=handle_socket_message, symbol=symbols[int(i)])
     socket.join()
 
 main()
+
+"""
+HEADINGS
+// Event type
+// Event time
+// Symbol
+// Aggregate trade ID
+// Price
+// Quantity
+// First trade ID
+// Last trade ID
+// Trade time
+// Is the buyer the market maker?
+// Ignore
+"""
