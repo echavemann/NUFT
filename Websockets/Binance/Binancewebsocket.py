8fb48bd3a9198b5baa2ec58cd9e244f520f7bf8e
jiale0402 <55820495+jiale0402@users.noreply.github.com>
2022-04-10 17:30:17 -0500

    Update Binancewebsocket.py

--- Websockets/Binance/Binancewebsocket.py
+++ Websockets/Binance/Binancewebsocket.py
@@ -1,5 +1,6 @@
 import time
 import pandas
+import s3test
 
 from binance import ThreadedWebsocketManager
 
@@ -9,37 +10,51 @@ api_secret = ''
 
 def main():
 
-    symbols = ['BTCUSDT','ETHUSDT','LUNAUSDT','SOLUSDT','AVAXUSDT','GLMRUSDT','FTMUSDT','VRAUSDT','PYRUSDT','DOGEUSDT']
+    # list of channels that we are subscribing to
+    symbols = ['BTCUSDT','ETHUSDT','LUNAUSDT','SOLUSDT','AVAXUSDT',
+    'GLMRUSDT','FTMUSDT','VRAUSDT','PYRUSDT','DOGEUSDT']
+    # initialize the websocket client
     socket = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
     socket.start()
 
+    # our handle method cannot take inputs other than message, so 
+    # variables have to be global. These variables exist for 
+    # storing response messages temporarily.
     global current_list_of_dict
     current_list_of_dict = []
     global dict_count
     dict_count = 0
-    global count
-    count = 0
 
+
+    # this is a handle method that's called every time that
+    # we receive a message from the websocket
     def handle_socket_message(msg):
-        # print(f"message type: {msg['e']}")
         global current_list_of_dict
         global dict_count
-        global count
+        # delete the useless ignore column, see below for a full
+        # list of headings of the response message
         del msg["M"]
         current_list_of_dict.append(msg)
         dict_count = dict_count + 1
+        # once we have received 100 messages from Binance, pack
+        # the messages and store it to a local csv file. Then it's
+        # uploaded to our AWS.
         if dict_count > 100: 
             dict_count = 0
             df = pandas.DataFrame(current_list_of_dict)
             current_list_of_dict = []
-            df.to_csv("TradaData"+str(count)+".csv")
-            count = count + 1
+            df.to_csv("TradeData.csv")
+            s3test.upload("TradeData.csv","nuft")
 
+    # subscribe to the Binance websocket channels according to the
+    # symbols listed above
     for i in range(len(symbols)):  
         socket.start_aggtrade_socket(callback=handle_socket_message, symbol=symbols[int(i)])
     socket.join()
 
 main()
+
+
 
 """
 HEADINGS
