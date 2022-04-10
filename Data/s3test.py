60f28ac0a9c7a09ea26bacfc3c7b5ac4cfb8c1bf
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2022-04-09 20:27:14 -0500

    Update s3test.py

--- Data/s3test.py
+++ Data/s3test.py
@@ -1,12 +1,26 @@
+
 import boto3
+import os
+import time
+import shutil
 
 KEY = ''
 SECRET = ''
 
-s3 = boto3.resource('s3')
-for bucket in s3.buckets.all():
-    print(bucket.name)
+client = boto3.client('s3',
+                      aws_access_key_id=KEY,
+                      aws_secret_access_key=SECRET)
 
-bucket = s3.bucket('nuft')
+def upload(name, bucket):
+    client.upload_file(name, bucket, name)
 
+def download(name, bucket,new_name):
+    client.download_file(bucket, name, new_name)
     
+def sync(source, destination, bucket):
+    download(source, bucket, 'temp')
+    shutil.copyfile(source, 'temp')
+    upload(destination, bucket)
+    os.remove('temp')
+    
+sync('s3test.py', 's3test.py', 'nuft')
