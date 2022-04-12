1df4e7840bc203cc90906be9b20a35de72df3dad
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2022-04-12 13:00:06 -0500

    user help

--- Keyreader.py
+++ Keyreader.py
@@ -1,11 +1,21 @@
+import enum
 import yaml
-#Github Copilot wrote this entire thing ngl.
 
 
 #Given string config_file pointing to the file, string service, and list of strings keys, 
 #Returns a list of values for the given keys for the given service.
+
+#Now with exceptions and better sanitizing!
 def get_keys(config_file, service, keys):
     with open(config_file, 'r') as stream:
         cfg = yaml.safe_load(stream)
     cfg = cfg[service]
-    return [cfg[key] for key in keys]
+    cfg = cfg.split()
+    for i, key in enumerate(keys):
+        string = key + ':'
+        t = cfg[i]
+        cfg[i] = cfg[i].replace(string,'')
+        if t == cfg[i]:
+            raise Exception('Key not found in config file!')
+        
+    return cfg
