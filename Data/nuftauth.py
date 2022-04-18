fde7a6b45c56e8e50de750ee1c3a1603d1886477
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2022-04-18 11:41:53 -0500

    Calling Program now deals with the client object.
    Pending actual testing...

--- Data/nuftauth.py
+++ Data/nuftauth.py
@@ -5,8 +5,8 @@ import os
 import time
 import shutil
 import yaml
+#aws globals
 
-#Building Block
 def get_keys(config_file, service, keys):
     with open(config_file, 'r') as stream:
         cfg = yaml.safe_load(stream)
@@ -28,34 +28,34 @@ def activate_aws(config_file):
                       aws_secret_access_key=SECRET)
     return client
 
+#Add this line to your program:
+client = activate_aws('config.yaml')
+    
+#[KEY,SECRET] = activate_aws('config.yaml')
+
 
 #uploads the file in path with name name to bucket bucket
-def upload(name, bucket):
-    client = activate_aws('config.yaml')
+def upload(client,name, bucket):
     client.upload_file(name, bucket, name)
 
 #Downloads the file named name from bucket bucket to an entry in folder named new_name
-def download(name, bucket,new_name):
-    client = activate_aws('config.yaml')
+def download(client, name, bucket,new_name):
     client.download_file(bucket, name, new_name)
 
 #Downloads a file and syncs your local file with it. Names must be the same. 
-def downsync(source, bucket):
-    client = activate_aws('config.yaml')
-    download(source, bucket, 'temp')
+def downsync(client,source, bucket):
+    download(client,source, bucket, 'temp')
     shutil.copyfile('temp', source)
     os.remove('temp')
 #Syncs cloud file with local file of the same name. Stores the file as 'log' so you can see what you overwrote.
-def upsync(source, bucket):
-    client = activate_aws('config.yaml')
-    download(source, bucket, 'log')
-    upload(source, bucket)
+def upsync(client,source, bucket):
+    download(client,source, bucket, 'log')
+    upload(client,source, bucket)
 #Call this function in your loop to store backups of the dynamic data which are named their timestamps.
-def store(name, bucket):
-    client = activate_aws('config.yaml')
+def store(client,name, bucket):
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     timern = str(datetime.date.today()) +' ' + str(current_time)
-    download(name, bucket, timern)
-    upload(timern, bucket)
+    download(client,name, bucket, timern)
+    upload(client,timern, bucket)
     os.remove(timern)
