eb27ba18faba681e9968c246fe7e78293bafc015
echavemann <echavemann@gmail.com>
2022-04-21 11:33:35 -0500

    update
    Level1 data is in, deleted bad files.

--- Data/Level1DataHook.py
+++ Data/Level1DataHook.py
@@ -16,13 +16,14 @@ global lst_of_ticker
 lst_of_ticker = []
 global batchSize
 batchSize = 0
-
+coins = ['BTC-USDT', 'ETH-USDT']
 async def main():
     global loop
         #This needs to be a dictionary powered by key piars     
         # callback function that receives messages from the socket
     async def handle_evt(msg):
-        if msg['topic'] == '/market/ticker:BTC-USDT':
+        coin = msg['subject']
+        if coin in coins:
             # print(f'got BTC-USDT tick:{msg["data"]}')
             global lst_of_ticker
             lst_of_ticker.append(msg)
@@ -31,12 +32,8 @@ async def main():
                 batchSize += 1
                 print(batchSize)
                 df = pd.DataFrame(lst_of_ticker)
-                df.to_csv('KucoinData.csv')
-                df.to_csv('/Users/frank/NUFT/KuCoin/BTCData.csv')
+                df.to_csv('level1.csv')
                 lst_of_ticker = []
-
-
-        #Store val in S3
         
 
     client = Client(api_key, api_secret, api_passphrase)
@@ -44,25 +41,12 @@ async def main():
     ksm = await KucoinSocketManager.create(loop, client, handle_evt)
 
     # await ksm.subscribe('/market/ticker:all')
-    await ksm.subscribe('/market/ticker:BTC-USDT')
+    await ksm.subscribe('/market/ticker:all')
 
     while True:
         print("sleeping to keep loop open")
-        await asyncio.sleep(20, loop=loop)
+        await asyncio.sleep(20)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-
-
-#Creating Session With Boto3.
-session = boto3.Session(
-    aws_access_key_id='<>',
-    aws_secret_access_key='<>'
-)
-
-#Creating S3 Resource From the Session.
-s3 = session.resource('s3')
-txt_data = b'Coin Data from KuCoin'
-object = s3.Object('<Coin Data>', 'file_name.txt')
-result = object.put(Body=txt_data)
