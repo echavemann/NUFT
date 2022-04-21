81ec0475e5b737b08b92709c13b31dee227d625d
echavemann <echavemann@gmail.com>
2022-04-20 21:51:01 -0500

    Update cloudbacktester.py

--- Algo/cloudbacktester.py
+++ Algo/cloudbacktester.py
@@ -28,10 +28,13 @@ for element in kcoins:
 delay = 0 #If we use delay, just need to stagger both the input of information and the order execution.
 pendings = []
 holdings = {}
+level1 = {}
 orderbook = {}
 pace = .1 #The pace of our tester. It is going to be 100ms by default, but maybe we should make it longer because Python is very slow. 
 balance = 100000
 allocation = 500
+
+
 
 
 #pull all of the data from websockets, parse, process, store in a dictionary so that the marketorders can maybe possibly run this in decent time. 
@@ -39,13 +42,14 @@ allocation = 500
 #every 100ms, check the pending orders file, if there are any, add them to the pendings list/stack. 
 
 #go through pendings, if the order is a sell but is not correlated with a holding, then just remove it. 
-
-#call marketorder on every single one, using market orders for everything for now. 
-
-#want to buy 5
-#orderbook only has 3
-#append same order to pendings with 5-3 quantity
-
+for entry in pendings:
+    #shit copilot is insane bro
+    if entry[1] == 'sell' and entry[0] not in holdings:
+        pendings.remove(entry)
+        
+        
+        
+        
 #Market order needs both the orders implemented but also we need to see what it's runtime is like, as well as hooking it up to the websocket data.
 def marketorder(symbol, quantity, side):
     if side == 'buy':
@@ -59,3 +63,12 @@ def marketorder(symbol, quantity, side):
 #1 - Trade failed due to insufficient funds
 #2 - Trade passed partially. 
 #3 - Trade failed due to insufficient funds
+
+
+#call marketorder on every single one, using market orders for everything for now. 
+for entry in pendings:
+    marketorder(entry[0], (allocation/level1[entry[0]]), entry[1])
+#want to buy 5
+#orderbook only has 3
+#append same order to pendings with 5-3 quantity
+
