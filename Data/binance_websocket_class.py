f4ee77a31190410febc97bdb900dab8f3fce91a6
jiale0402 <55820495+jiale0402@users.noreply.github.com>
2022-05-07 21:17:19 -0500

    Update binance_websocket_class.py

--- Data/binance_websocket_class.py
+++ Data/binance_websocket_class.py
@@ -1,4 +1,4 @@
-import multiprocessing as mp
+import pandas as pd
 from binance import ThreadedWebsocketManager
 
 class Binance_Websocket:
@@ -18,28 +18,18 @@ class Binance_Websocket:
 	    # our handle method cannot take inputs other than message, so 
 	    # variables have to be global. These variables exist for 
 	    # storing response messages temporarily.
-	    global current_list_of_dict
-	    current_list_of_dict = []
-	    global dict_count
-	    dict_count = 0
 
 	    # this is a handle method that's called every time that
 	    # we receive a message from the websocket
 	    def handle_socket_message(msg):
-	        global current_list_of_dict
-	        global dict_count
 	        # delete the useless ignore column, see below for a full
 	        # list of headings of the response message
 	        del msg["M"]
-	        current_list_of_dict.append(msg)
-	        dict_count = dict_count + 1
 	        # once we have received 100 messages from Binance, pack
 	        # the messages and store it to a local csv file. Then it's
 	        # uploaded to our AWS.
-	        if dict_count > 100: 
-	            dict_count = 0
-	            df = pandas.DataFrame(current_list_of_dict)
-	            self.queue.put(df)
+	        df = pandas.DataFrame(msg)
+	        self.queue.put(df)
 
 	    # subscribe to the Binance websocket channels according to the
 	    # symbols listed above
