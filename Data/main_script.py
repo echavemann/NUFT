6086713e303f82e74f2607a91535c3a62b1bc8de
jiale0402 <55820495+jiale0402@users.noreply.github.com>
2022-05-07 21:55:47 -0500

    not really fixing the problem, in progress

--- Data/main_script.py
+++ Data/main_script.py
@@ -1,19 +1,21 @@
 import multiprocessing as mp
 import binance_websocket_class as bwc
 import pandas as pd
+import queue_fix
 
 if __name__ == '__main__':
-	q = mp.Queue()
+	mp.set_start_method('spawn')
+	q = queue_fix.Queue()
 	websocket = bwc.Binance_Websocket(q,['BTCUSDT'])
 	p = mp.Process(target=websocket.main(), args=(q))
 	p.start()
 
-import multiprocessing as mp
-q = mp.Queue()
-q.put(2)
-q.put(4)
+	q = mp.Queue()
+	q.put(2)
+	q.put(4)
 
-elements = list()
-while q.qsize():
-    elements.append(q.get())
-print(elements)
+	elements = list()
+	while q.qsize():
+	    elements.append(q.get())
+
+	print(elements)
