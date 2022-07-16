ab13a8c7ea2cec160ed16ca2a47aee7230749520
Ethan Havemann <echavemann@gmail.com>
2022-07-15 21:57:07 -0500

    running!

--- Infra/Kraken_Websocket.py
+++ Infra/Kraken_Websocket.py
@@ -7,18 +7,21 @@ import json
 # Creating Kraken websocket class
 class kraken_websocket_raw():
 
-    def __init__(self, queue, socket, ids = [], channels = []):
+    def __init__(self, queue, socket, coins = []):
         self.queue = queue
         self.socket = socket
-        self.ids = ids
-        self.channels = channels
+        self.coins = coins
         self.sub_message = self.on_open()
     
     def on_open(self): # Generates a subscribe message to be converted into json to be sent to endpoint
         subscribe_message = {}
-        subscribe_message["type"] = "subscribe"
-        subscribe_message["product_ids"] = self.ids
-        subscribe_message["channels"] = self.channels
+        subscribe_message["event"] = "subscribe"
+        subscribe_message["subscription"] = {"name":"trade"}
+        pairs = []
+        for coin in self.coins:
+            pairs.append(coin)
+        subscribe_message["pair"] = pairs
+        print(subscribe_message)
         return subscribe_message
     
     async def run(self): #on_message, sends json subscription to endpoint and awaits response to be put into queue
@@ -28,7 +31,7 @@ class kraken_websocket_raw():
                 while True:
                     message = await websocket.recv()
                     self.queue.put(message)
-                    print('Kraken Message Received')
+                    print(message)
         except Exception:
             import traceback
             print(traceback.format_exc())
@@ -38,10 +41,9 @@ class kraken_websocket_raw():
     
 async def main(): 
     q = multiprocessing.Queue()
-    ids = ['BTC-USD','ETH-USD']
-    channels = ["ticker"]
+    coins = ["XBT/USD","XRP/USD"]
     socket = 'wss://ws.kraken.com/'
-    kws = kraken_websocket_raw(q,socket,ids,channels)
+    kws = kraken_websocket_raw(q,socket,coins)
     await kws.run()
 
 # Notice: Non-Async Wrapper is required for multiprocessing to run
