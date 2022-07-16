adf08dedef541fbd37e2a010ff6fceb50f624747
pl712 <liuzhengyao2011@gmail.com>
2022-07-16 11:39:52 -0700

    Update Kucoin_Websocket.py

--- Infra/Kucoin_Websocket.py
+++ Infra/Kucoin_Websocket.py
@@ -4,38 +4,54 @@ import requests
 import multiprocessing as mp
 import json
 from uuid import uuid4
+import time
 
-class kucoin_websocket():
-    def __init__(self, queue, topics = []):
+'''
+Usage Instructions:
+    1.  from Kucoin_Websocket import kuCoin_WS
+    2.  kuCoin_WS(
+            {queue_name}, type is mp.Queue(),
+            {tickers}, type is python list w/ multiple items separated by comma
+        )
+'''
+
+class kuCoin_WS():
+    def __init__(self, queue:mp.Queue(), topics:list) -> None:
         self.queue = queue
         self.topics = topics
+        self.start = time.time()
         
-        self.sessionID, self.wsEndPoint, self.timeOut = self._getWSEndpoint()
-        self.MSG = self._getMSG()
+        self.sessionID, self.wsEndPoint, self.timeOut, self.MSG = self._getWSEndpoint()
         asyncio.run(self._run())
 
-    def _getWSEndpoint(self):
+    def _getWSEndpoint(self) -> tuple:
         x = requests.post('https://api.kucoin.com/api/v1/bullet-public').json()
         token = x['data']['token']
         server = x['data']['instanceServers'][0]["endpoint"]
         id = str(uuid4()).replace('-', '')
-        return str(uuid4()).replace('-', ''), server + '?token=' + token + '&[connectId=' + id + ']', int(x['data']['instanceServers'][0]['pingInterval'] / 1000) - 1
 
-    def _getMSG(self):
         temp = []
         for i in self.topics:
-            temp.append({'id': self.sessionID, 'type': 'subscribe', 'topic': i, 'response': True})
-        return temp
-    
-    async def _run(self):
+            temp.append({'id': id, 'type': 'subscribe', 'topic': i, 'response': True})
+
+        return str(uuid4()).replace('-', ''), server + '?token=' + token + '&[connectId=' + id + ']', int(x['data']['instanceServers'][0]['pingInterval'] / 1000) - 1, temp
+
+    def _addToQueue(self, q:mp.Queue(), data:str) -> None:
+        if not q.full():
+            q.put(data)
+            print(data)
+        else:
+            q.get()
+            q.put(data)
+            print(data)
+
+    async def _run(self) -> None:
         async with websockets.connect(self.wsEndPoint, ping_interval=self.timeOut, ping_timeout=None) as websocket:
             for i in self.MSG:
                 await websocket.send(json.dumps(i))
+            
             while True:
                 message = await websocket.recv()
-                if self.queue.full():
-                    print("Queue is Full")
-                self.queue.put(message)
-                print(message)
+                self._addToQueue(self.queue, message)
 
-a = kucoin_websocket(mp.Queue(), ['/market/ticker:all','/market/level2:BTC-USDT'])
+kuCoin_WS(mp.Queue(), ['/market/ticker:all','/market/level2:BTC-USDT'])
