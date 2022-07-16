541e89fd23cf513381ff5f3e417fae9f5426259c
Richard Bann <richard@bann.com>
2022-07-15 23:51:30 -0300

    Update Kraken_Websocket.py

--- Infra/Kraken_Websocket.py
+++ Infra/Kraken_Websocket.py
@@ -1,27 +1,34 @@
 #Dependencies
 import asyncio
 import websockets
-import requests
 import multiprocessing
 import json
 
 # Creating Kraken websocket class
 class kraken_websocket_raw():
 
-    def __init__(self, queue, coins):
+    def __init__(self, queue, socket, ids = [], channels = []):
         self.queue = queue
-        self.coins = coins
-        self.socket = 'wss://ws.kraken.com/'
-
+        self.socket = socket
+        self.ids = ids
+        self.channels = channels
+        self.sub_message = self.on_open()
+    
+    def on_open(self): # Generates a subscribe message to be converted into json to be sent to endpoint
+        subscribe_message = {}
+        subscribe_message["type"] = "subscribe"
+        subscribe_message["product_ids"] = self.ids
+        subscribe_message["channels"] = self.channels
+        return subscribe_message
     
     async def run(self): #on_message, sends json subscription to endpoint and awaits response to be put into queue
         try:
             async with websockets.connect(self.socket) as websocket:
-                await websocket.send('{"event":"subscribe", "subscription":{"name":"trade"}, "pair":["XBT/USD","XRP/USD"]}')
+                await websocket.send(json.dumps(self.sub_message))
                 while True:
                     message = await websocket.recv()
                     self.queue.put(message)
-                    print('Kraken Data Received')
+                    print('Kraken Message Received')
         except Exception:
             import traceback
             print(traceback.format_exc())
@@ -31,11 +38,13 @@ class kraken_websocket_raw():
     
 async def main(): 
     q = multiprocessing.Queue()
-    cwr = Kraken_Websocket(q,[])
-    await cwr.run()
+    ids = ['BTC-USD','ETH-USD']
+    channels = ["ticker"]
+    socket = 'wss://ws.kraken.com/'
+    kws = kraken_websocket_raw(q,socket,ids,channels)
+    await kws.run()
 
 # Notice: Non-Async Wrapper is required for multiprocessing to run
 def run():
     asyncio.run(main())
-
 run()
