d1cf87d65ea7d4a54a4c6447f253542535abd820
Jason <jasonchicago@att.net>
2022-07-24 09:15:04 -0500

    Click Based CLI & Deprecation

--- Infra/CLI.py
+++ Infra/CLI.py
@@ -1,61 +1,77 @@
-# Command Line Interface 
-import argparse
-import sys
-import Coinbase_Websocket
-import shlex
-from main_script import activate
+import click
 
-# Command Line Interface Class 
-class CLI():
-
-    def __init__(self, argString):
-        # Defines the list of commands available to the user
-        self.commands = {
-        "run" : "Run",
-        #Starts all websockets, does not return control flow until all websockets are open. 
-        "stop" : "Stop",
-        #Stops all websockets, keeps connection open. 
-        "backtest" : "Start Backtest", 
-        #Takes in algorithm path, historical/realtime toggle, and start/end date if historical. Executes backtest. Returns control flow during backtest, but will autoprint result.  
-        "systat" : "System Status",
-        #Prints system status, operational and non-operational status of all websockets.
-        "exchstat" : "Algo Status",
-        #Prints performance of all algorithms.
-        "query" : "Query memory",
-        #Given query for backtesting report(s), returns backtesting report(s).
-        "train" : "Passing Data ML Training"}
-        #Given ML algorithm path, data query, finds data, trains model, returns control flow during train, but will autoprint result.
-        self.parser = argparse.ArgumentParser(description="NUFT Command Line Interface")
-        self.argString = argString
+# NUFT Command Line Interface
     
-    # Add arguments such as positional or flag args here 
-    def parse(self):
-        #Main Command
-        self.parser.add_argument("cmd", type=str, choices=self.commands, help="Type a cmd from the list of available commands")
+# Dictionary of the list of commands available to the user
+# Not used by Click CLI 
+commands = {
+"start" : "Start",
+#Starts all websockets, does not return control flow until all websockets are open. 
+"stop" : "Stop",
+#Stops all websockets, keeps connection open. 
+"backtest" : "Start Backtest", 
+#Takes in algorithm path, historical/realtime toggle, and start/end date if historical. Executes backtest. Returns control flow during backtest, but will autoprint result.  
+"systat" : "System Status",
+#Prints system status, operational and non-operational status of all websockets.
+"exchstat" : "Algo Status",
+#Prints performance of all algorithms.
+"query" : "Query memory",
+#Given query for backtesting report(s), returns backtesting report(s).
+"train" : "Passing Data ML Training"}
+#Given ML algorithm path, data query, finds data, trains model, returns control flow during train, but will autoprint result.
 
-        parsed_args = self.parser.parse_args(shlex.split(self.argString))
-        return parsed_args
+# Defines the main command group
+@click.group()
+def CLI():
+    """
+    Simple CLI built for Northwestern's FinTech Club
+    """
+    pass
 
-    # Determines the parsed command and runs the appropriate funciton 
-    def execute_command(self):
-        cmd = self.parse().cmd
-        if (cmd == "run"):
-            activate()
-            #Asyncio Run?
-        elif (cmd == "stop"):
-            print("stop")
-        elif (cmd == "backtest"):
-            print("backtest")
-        elif (cmd == "systat"):
-            print("systat")
-        elif (cmd == "exchstat"):
-            print("exchstat")
-        elif (cmd == "train"):
-            print("train")
+# Start Command
+@CLI.command(help="Starts all websockets, does not return control flow until all websockets are open.")
+def start():
 
-# Running the CLI, takes in an argString (To be input from GUI)           
-def run(str):
-    cli = CLI(str)
-    cli.execute_command()
+    click.echo("Starting Stream")
 
-run("stop")
+
+# Stop Command
+@CLI.command(help="Stops all websockets, keeps connection open.")
+def stop():
+
+    click.echo("Stopping Stream")
+
+
+# backtest Command
+@CLI.command(help="Takes in algorithm path, historical/realtime toggle, and start/end date if historical. Executes backtest. Returns control flow during backtest, but will autoprint result. ")
+def backtest():
+
+    click.echo("Running Backtester")
+
+
+@CLI.command(help="Prints system status, operational and non-operational status of all websockets.")
+def systat():
+
+    click.echo("Displaying System Status")
+
+
+@CLI.command(help="Prints performance of all algorithms.")
+def excstat():
+
+    click.echo("Displaying Exchange Status")
+
+
+@CLI.command(help="Given query for backtesting report(s), returns backtesting report(s).")
+def query():
+
+    click.echo("Querying Data")
+
+
+@CLI.command(help="Given ML algorithm path, data query, finds data, trains model, returns control flow during train, but will autoprint result.")
+def train():
+
+    click.echo("Training Models")
+
+CLI()
+
+
