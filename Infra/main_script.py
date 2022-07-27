8128ee1c18ab592bd905f3bdebf78a1708664f1b
dpark4 <95252586+dpark4@users.noreply.github.com>
2022-07-26 20:36:29 -0500

    wip lol

--- Infra/main_script.py
+++ Infra/main_script.py
@@ -16,20 +16,24 @@ async def main(coins):
 	#NO SEMICOLONS!!
 		q1 = mp.Queue()
 		q2 = mp.Queue()
+		df = pd.DataFrame(data = [])
 		# kr = kr.Kraken_Websocket(coins)
 		# cb = cb.Coinbase_Websocket(coins)
 		ks_ws = ks.Kucoin_Websocket(q1, q2, topics = ['/market/ticker:' + ','.join(coins), '/market/level2:' + ','.join(coins)])
-		while q1.not_empty():
-			last_list = q1.get()[-9:]
-			print(last_list)
-		
 		# bc = bc.Binance_Websocket(coins)
 		# gm = gm.Gemini_Websocket(coins)
+		# temp_q1_rows = q1.get()
+		# print(temp_q1_rows)
+		# for row in temp_q1_rows:
+		# 	pd.concat(df, row)
+		# print(df)
 		executor.submit(ks_ws._run_)
-		executor.submit(cb.run)
-		executor.submit(bc.run)
-		executor.submit(kr.run)
-		executor.submit(gm.run)
+		executor.submit(print(q1.get()))
+
+		# executor.submit(cb.run)
+		# executor.submit(bc.run)
+		# executor.submit(kr.run)
+		# executor.submit(gm.run)
 
 #Run code
 coins = ['BTH-USDT']
