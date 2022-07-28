e202d432ab4bbc567fba87a761a8697ddf5f28de
Jason <jasonchicago@att.net>
2022-07-28 14:47:40 -0500

    Refactored Infra Directory

--- Infra/Kraken /Kraken_Websocket.py
+++ Infra/Kraken /Kraken_Websocket.py
@@ -1,29 +1,31 @@
-#Dependencies
+# Dependencies
 import asyncio
 import websockets
 import multiprocessing
 import json
 
 # Creating Kraken websocket class
+
+
 class kraken_websocket_raw():
 
-    def __init__(self, queue, socket, coins = []):
+    def __init__(self, queue, socket, coins=[]):
         self.queue = queue
         self.socket = socket
         self.coins = coins
         self.sub_message = self.on_open()
-    
-    def on_open(self): # Generates a subscribe message to be converted into json to be sent to endpoint
+
+    def on_open(self):  # Generates a subscribe message to be converted into json to be sent to endpoint
         subscribe_message = {}
         subscribe_message["event"] = "subscribe"
-        subscribe_message["subscription"] = {"name":"ticker"}
+        subscribe_message["subscription"] = {"name": "ticker"}
         pairs = []
         for coin in self.coins:
             pairs.append(coin)
         subscribe_message["pair"] = pairs
         return subscribe_message
-    
-    async def run(self): #on_message, sends json subscription to endpoint and awaits response to be put into queue
+
+    async def run(self):  # on_message, sends json subscription to endpoint and awaits response to be put into queue
         try:
             async with websockets.connect(self.socket) as websocket:
                 await websocket.send(json.dumps(self.sub_message))
@@ -37,13 +39,19 @@ class kraken_websocket_raw():
 
     def start(self):
         self.run()
-    
-async def main(coins): 
+
+
+async def main(coins):
     q = multiprocessing.Queue()
     socket = 'wss://ws.kraken.com/'
-    kws = kraken_websocket_raw(q,socket,coins)
+    kws = kraken_websocket_raw(q, socket, coins)
     await kws.run()
 
 # Notice: Non-Async Wrapper is required for multiprocessing to run
-def run(coins = ["XBT/USD","XBT/EUR"]):
+
+
+def run(coins=["XBT/USD", "XBT/EUR"]):
     asyncio.run(main(coins))
+
+
+run()
--- Infra/main_script.py
+++ Infra/main_script.py
@@ -1,56 +1,66 @@
-import multiprocessing as mp
 import concurrent.futures as cf
-import Binance_Websocket_Formatted as bc
-# from Infra.Binance_Websocket import Binance_Websocket
-# from Infra.Coinbase_Websocket import Coinbase_Websocket
-import Kucoin_Websocket_Formatted as ks
-import Coinbase_Websocket as cb
-import Kraken_Websocket as kr
-import Gemini_Websocket as gm
+import multiprocessing as mp
+import Kucoin.Kucoin_Websocket_Formatted as ks
+import Coinbase.Coinbase_Websocket as cb
+import Gemini.Gemini_Websocket as gm
+import Binance.Binance_Websocket_Formatted as bc
 import pandas as pd
 import asyncio
 
-#All functions imported need to be non-async! If you commit async functions, our code will not work, and we will have an emergency debugging in your 'honor'.
 
-#Stage code
+# from Infra.Binance_Websocket import Binance_Websocket
+# from Infra.Coinbase_Websocket import Coinbase_Websocket
+
+# All functions imported need to be non-async! If you commit async functions, our code will not work, and we will have an emergency debugging in your 'honor'.
+
+# Stage code
+
+
 async def main(coins):
-	with cf.ProcessPoolExecutor(max_workers=mp.cpu_count()) as executor:
-	#NO SEMICOLONS!!
-		q1 = mp.Queue()
-		q2 = mp.Queue()
-		df = pd.DataFrame(data = [])
+    with cf.ProcessPoolExecutor(max_workers=mp.cpu_count()) as executor:
+        # NO SEMICOLONS!!
+        q1 = mp.Queue()
+        q2 = mp.Queue()
+        df = pd.DataFrame(data=[])
 
-		#Stage 1: setting up all the websockets
-		binance = bc.Binance_Websocket(q1, coins)
-		coinbase = cb.Coinbase_Websocket(q1, 'wss://ws-feed.exchange.coinbase.com', coins, channels = ["ticker"])
-		kraken = kr.Kraken_Websocket(q1, q2, topics = ['/market/ticker:all', '/market/level2:BTC-USDT'])
-		gemini = gm.Gemini_Websocket(q1, q2, 'wss://api.gemini.com/v1/multimarketdata?symbols=' + ','.join(coins) , 'wss://api.gemini.com/v2/marketdata', coins)
-		ks_ws = ks.Kucoin_Websocket(q1, q2, topics = ['/market/ticker:' + ','.join(coins), '/market/level2:' + ','.join(coins)])
-		# temp_q1_rows = q1.get()
-		# print(temp_q1_rows)
-		# for row in temp_q1_rows:
-		# 	pd.concat(df, row)
-		# print(df)
+        # Stage 1: setting up all the websockets
+        binance = bc.Binance_Websocket(q1, coins)
+        coinbase = cb.Coinbase_Websocket(
+            q1, 'wss://ws-feed.exchange.coinbase.com', coins, channels=["ticker"])
+        kraken = kr.Kraken_Websocket(
+            q1, q2, topics=['/market/ticker:all', '/market/level2:BTC-USDT'])
+        gemini = gm.Gemini_Websocket(q1, q2, 'wss://api.gemini.com/v1/multimarketdata?symbols=' +
+                                     ','.join(coins), 'wss://api.gemini.com/v2/marketdata', coins)
+        ks_ws = ks.Kucoin_Websocket(q1, q2, topics=[
+                                    '/market/ticker:' + ','.join(coins), '/market/level2:' + ','.join(coins)])
+        # temp_q1_rows = q1.get()
+        # print(temp_q1_rows)
+        # for row in temp_q1_rows:
+        # 	pd.concat(df, row)
+        # print(df)
 
-		#Stage 2: running all the websockets
-		try:
-			executor.submit(ks_ws._run_)
-		except Exception:
-			try:
-				executor.submit(coinbase.run)
-			except Exception:
-				try:
-					executor.submit(binance.run)
-				except Exception:
-					try:
-						executor.submit(kraken.run)
-					except Exception:
-						executor.submit(gemini.run)
+        # Stage 2: running all the websockets
+        try:
+            executor.submit(ks_ws._run_)
+        except Exception:
+            try:
+                executor.submit(coinbase.run)
+            except Exception:
+                try:
+                    executor.submit(binance.run)
+                except Exception:
+                    try:
+                        executor.submit(kraken.run)
+                    except Exception:
+                        executor.submit(gemini.run)
 
-#Run code
+# Run code
 coins = ['BTH-USDT']
+
+
 def activate(coins):
-	if __name__ == '__main__':
-		asyncio.get_event_loop().run_until_complete(main(coins))
+    if __name__ == '__main__':
+        asyncio.get_event_loop().run_until_complete(main(coins))
+
 
 activate(coins)
