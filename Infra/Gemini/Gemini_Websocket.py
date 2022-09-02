7b2d553fcb6b011bdf37a2e00baf170c2c3f590a
dpark4 <95252586+dpark4@users.noreply.github.com>
2022-09-02 22:45:32 +0900

    Update Gemini_Websocket.py

--- Infra/Gemini/Gemini_Websocket.py
+++ Infra/Gemini/Gemini_Websocket.py
@@ -20,8 +20,8 @@ class Gemini_Websocket():
     #generates a subscribe message to be converted into json to be sent to endpoint
     def on_open(self):
         subscribe_message = {}
-        subscribe_message["type"] = "subscribe"
-        subscribe_message["subscriptions"] = [{"name":"l2","symbols":self.coins}]
+        subscribe_message['type'] = 'subscribe'
+        subscribe_message['subscriptions'] = [{'name':'l2','symbols':self.coins}]
         return subscribe_message
     
     async def run(self): #on_message, sends json subscription to endpoint and awaits response to be put into queue
@@ -30,28 +30,24 @@ class Gemini_Websocket():
                 await websocket.send(json.dumps(self.sub_message))
                 # await websocket.send(json.dumps(self.update_message))  #Takes around 20 seconds for update messages to show up...
                 while True:
-                    ### 07/22/2022
-                    ### Formatting Stuff Starts Here
-                    # Receive Message
                     message = await websocket.recv()
-                    #Convert message to json
                     temp_json = json.loads(message)
-                    # Set variables that will be entered into DataFrame
                     msg_data = []
                     time_id = []
                     curr_dt = None
-
                     # if the request is of type l2_updates (message for lvl2 market data), get the DateTime from the json
-                    if temp_json['type'] == 'l2_updates':
+                    if 'result' in temp_json.keys() and temp_json['result'] == 'error':
+                        pass
+                    elif temp_json['type'] == 'l2_updates':
                         if 'trades' in temp_json.keys():
                         #check to see if timestamp is available to get
-                            if "timestamp" in temp_json.get("trades")[0].keys():
+                            if 'timestamp' in temp_json.get('trades')[0].keys():
                                 curr_dt = datetime.fromtimestamp(time.time())  
                                 # print(temp_json)
                                 # curr_dt = datetime.utcfromtimestamp(temp_json.get("trades")[0].get("timestamp")/1000).strftime('%Y-%m-%d %H:%M:%S')
                                 msg_data = {
                                     'exchange': 'gemini',
-                                    # 'type': 'l2_updates',   
+                                    'type': 'l2update',   
                                     'ticker': temp_json['symbol'],
                                     'side': temp_json['changes'][0][0],
                                     'price': temp_json['changes'][0][1],
@@ -59,28 +55,35 @@ class Gemini_Websocket():
                                 }
                                 # Prep index for DataFrame
                                 time_id = [curr_dt]
-                                if self.queue_1.full():
-                                    print("working")
+                                if self.queue_2.full():
+                                    print('working')
                                 if msg_data != [] and time_id != []:
                                     # Create DataFrame
                                     df = pandas.DataFrame(data = msg_data, index=time_id)
                                     print(df)
                                     # put the dataframe into the queue
                                     self.queue_2.put(df)
-                    # elif temp_json['type'] == 'trade':
-                    #     curr_dt = datetime.utcfromtimestamp(temp_json["timestamp"]/1000).strftime('%Y-%m-%d %H:%M:%S')
-                    #     # print(temp_json)
-                    #     msg_data = {
-                    #             'exchange': 'Gemini',
-                    #             'type' : 'trade',
-                    #             'symbol': temp_json['symbol'],
-                    #             'action': temp_json['side'],
-                    #             'price': temp_json['price'],
-                    #             'quantity': temp_json['quantity']
-                    #         }
-                    #         # Prep index for DataFrame
-                        # time_id = [curr_dt]
-                        # print('GOGOGO')
+                    elif temp_json['type'] == 'trade':
+                        curr_dt = datetime.utcfromtimestamp(temp_json['timestamp']/1000).strftime('%Y-%m-%d %H:%M:%S')
+                        # print(temp_json)
+                        msg_data = {
+                                'exchange': 'Gemini',
+                                'type': 'trade',
+                                'ticker': temp_json['symbol'],
+                                'side': temp_json['side'],
+                                'price': temp_json['price'],
+                                'quantity': temp_json['quantity']
+                            }
+                            # Prep index for DataFrame
+                        time_id = [curr_dt]
+                        if self.queue_2.full():
+                            print('working')
+                        if msg_data != [] and time_id != []:
+                            # Create DataFrame
+                            df = pandas.DataFrame(data = msg_data, index = time_id)
+                            print(df)
+                            # put the dataframe into the queue
+                            self.queue_2.put(df)
                 
         except Exception:
             import traceback
@@ -96,6 +99,6 @@ async def main(coins):
     await gws.run()
 
 # Notice: Non-Async Wrapper is required for multiprocessing to run
-def run(coins = ["BTCUSD", "ETHUSD"]):
+def run(coins = ['BTCUSD', 'ETHUSD']):
     asyncio.run(main(coins))
 run()
