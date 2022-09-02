88df351b98c53818e20c4df6cd314faa62fbd9b0
dpark4 <95252586+dpark4@users.noreply.github.com>
2022-09-02 23:32:52 +0900

    Update Gemini_Websocket.py

--- Infra/Gemini/Gemini_Websocket.py
+++ Infra/Gemini/Gemini_Websocket.py
@@ -14,20 +14,24 @@ class Gemini_Websocket():
         self.queue_1 = queue_1
         self.queue_2 = queue_2
         self.coins = coins
-        self.sub_message = self.on_open()
+        self.sub_msg_1 = self.on_open()[0]
+        self.sub_msg_1 = self.on_open[1]
         # self.update_message = self.update_response()
 
     #generates a subscribe message to be converted into json to be sent to endpoint
     def on_open(self):
-        subscribe_message = {}
-        subscribe_message['type'] = 'subscribe'
-        subscribe_message['subscriptions'] = [{'name':'l2','symbols':self.coins}]
-        return subscribe_message
+        sub_msg_1 = {}
+        sub_msg_1['type'] = 'subscribe'
+        sub_msg_1['subscriptions'] = [{'name':'l2','symbols':self.coins}]
+        # sub_msg_2 = {}
+        # sub_msg_2['type'] = 'subscribe'
+        # sub_msg_2['subscriptions'] = [{'name':'l1','symbols':self.coins}]
+        return [sub_msg_1]
     
     async def run(self): #on_message, sends json subscription to endpoint and awaits response to be put into queue
         try:
             async with websockets.connect('wss://api.gemini.com/v2/marketdata') as websocket:
-                await websocket.send(json.dumps(self.sub_message))
+                await websocket.send(json.dumps(self.sub_msg_1))
                 # await websocket.send(json.dumps(self.update_message))  #Takes around 20 seconds for update messages to show up...
                 while True:
                     message = await websocket.recv()
@@ -41,28 +45,26 @@ class Gemini_Websocket():
                     elif temp_json['type'] == 'l2_updates':
                         if 'trades' in temp_json.keys():
                         #check to see if timestamp is available to get
-                            if 'timestamp' in temp_json.get('trades')[0].keys():
-                                curr_dt = datetime.fromtimestamp(time.time())  
-                                # print(temp_json)
-                                # curr_dt = datetime.utcfromtimestamp(temp_json.get("trades")[0].get("timestamp")/1000).strftime('%Y-%m-%d %H:%M:%S')
-                                msg_data = {
-                                    'exchange': 'gemini',
-                                    'type': 'l2update',   
-                                    'ticker': temp_json['symbol'],
-                                    'side': temp_json['changes'][0][0],
-                                    'price': temp_json['changes'][0][1],
-                                    'quantity': temp_json['changes'][0][2]
-                                }
+                            # if 'timestamp' in temp_json.get('trades')[0].keys():
+                            curr_dt = datetime.fromtimestamp(temp_json['trades'][0]['timestamp']/1000).strftime('%Y-%m-%d %H:%M:%S')
+                            msg_data = {
+                                'exchange': 'gemini',
+                                'type': 'l2update',   
+                                'ticker': temp_json['symbol'],
+                                'side': temp_json['changes'][0][0],
+                                'price': temp_json['changes'][0][1],
+                                'quantity': temp_json['changes'][0][2]
+                            }
                                 # Prep index for DataFrame
-                                time_id = [curr_dt]
-                                if self.queue_2.full():
-                                    print('working')
-                                if msg_data != [] and time_id != []:
-                                    # Create DataFrame
-                                    df = pandas.DataFrame(data = msg_data, index=time_id)
-                                    print(df)
-                                    # put the dataframe into the queue
-                                    self.queue_2.put(df)
+                            time_id = [curr_dt]
+                            if self.queue_2.full():
+                                print('working')
+                            if msg_data != [] and time_id != []:
+                                # Create DataFrame
+                                df = pandas.DataFrame(data = msg_data, index=time_id)
+                                print(df)
+                                # put the dataframe into the queue
+                                self.queue_2.put(df)
                     elif temp_json['type'] == 'trade':
                         curr_dt = datetime.utcfromtimestamp(temp_json['timestamp']/1000).strftime('%Y-%m-%d %H:%M:%S')
                         # print(temp_json)
